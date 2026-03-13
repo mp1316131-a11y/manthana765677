edc1602164b9a8b9ab0aedb23cc5fbb4ea6c032c
mp1316131-a11y <mp1316131@gmail.com>
2026-03-13 15:23:34 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,30 @@
+class exmaple 
+{
+public static void main(string a[])
+{
+int marks;
+scanner sc=new scanner (System.in);
+System.out .println("enter the marks")
+marks=sc.nextInt();
+}
+if (marks<35);
+System.out.println("fail")
+}
+else if (marks>=35 &&marks<50);
+{
+System.out.println("third class");
+}
+else if (marks>=&&marks<60);
+{
+System.out.println("second class");
+}
+else if (marks>=60 && marks 75);
+{
+System.out.println("first class");
+}
+else:
+{
+System.out.println("distinction");
+}
+}
+}
